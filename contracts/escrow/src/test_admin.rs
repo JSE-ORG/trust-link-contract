78a767073a78529afcfbf76deb4faa5828194edf
akprinciple <akeemolayiwola9@gmail.com>
2026-08-30 23:00:45 +0100

    fix: improve formatting of platform fee cap test assertion

--- contracts/escrow/src/test_admin.rs
+++ contracts/escrow/src/test_admin.rs
@@ -96,9 +96,11 @@ fn test_set_platform_fee_cap_matches_production_limit() {
     let (_contract_id, client, admin, _fee_collector) = setup_contract(&env);
 
     client.set_platform_fee(&admin, &200_u32);
-    assert_eq!(client.try_set_platform_fee(&admin, &201_u32), Err(Ok(ContractError::PlatformFeeExceedsMax)));
+    assert_eq!(
+        client.try_set_platform_fee(&admin, &201_u32),
+        Err(Ok(ContractError::PlatformFeeExceedsMax))
+    );
 }
-
 
 #[test]
 fn test_upgrade() {
